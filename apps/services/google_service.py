e5b2065bc2424bd73c46286aae9782689b01fc6c
Albert Dale Palacio <palacioalbertdale@gmail.com>
2024-05-03 17:35:08 +0800

    Added temp Generative AI support

--- apps/services/google_service.py
+++ apps/services/google_service.py
@@ -1,7 +1,7 @@
-import secrets
+import json
 import gspread
 
-from flask import url_for
+import google.generativeai as genai
 
 from gspread.utils import GridRangeType
 
@@ -21,12 +21,29 @@ REFRESHER_ANSWER_2_INDEX = 5
 REFRESHER_QUESTION_3_INDEX = 6
 REFRESHER_ANSWER_3_INDEX = 7
 
+GEMINI_API_KEY = ''
 
-def get_context():
-    return None
+with open("gemini.json") as jsonFile:
+    GEMINI_API_KEY = json.load(jsonFile)['key']
+    genai.configure(api_key=GEMINI_API_KEY)
+
+model = genai.GenerativeModel('gemini-pro')
+
+response = model.generate_content("How old was Don Bosco when he died, what is the latest news of the Salesian brotherhood?")
+
+print(response.text)
+
+def get_question_sets():
+    question_sets = []
+    for item in listOfLists:
+        question_sets.append({'question': item[REFRESHER_QUESTION_1_INDEX], 'correct_answer': item[REFRESHER_ANSWER_1_INDEX]})
+        question_sets.append({'question': item[REFRESHER_QUESTION_2_INDEX], 'correct_answer': item[REFRESHER_ANSWER_2_INDEX]})
+        question_sets.append({'question': item[REFRESHER_QUESTION_3_INDEX], 'correct_answer': item[REFRESHER_ANSWER_3_INDEX]})
+    return question_sets
 
 def get_learnings():
     return_value = []
     for item in listOfLists:
         return_value.append(item[SUMMARY_INDEX])
     return return_value
+    
